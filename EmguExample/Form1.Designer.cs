158d1a936351abb82731c0e267713adc7f2c44a8
Raymond Smit <rsmit@ltu.edu>
2018-04-30 16:08:25 -0400

    Add project files.

---
+++ EmguExample/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace EmguExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.slider = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.slider2 = new System.Windows.Forms.TrackBar();
+            this.slider3 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(510, 355);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // slider
+            // 
+            this.slider.Location = new System.Drawing.Point(528, 12);
+            this.slider.Maximum = 255;
+            this.slider.Name = "slider";
+            this.slider.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.slider.Size = new System.Drawing.Size(56, 355);
+            this.slider.TabIndex = 2;
+            this.slider.Value = 165;
+            this.slider.Scroll += new System.EventHandler(this.slider_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(591, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Threshold";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(591, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "[val]";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 373);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(510, 355);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // slider2
+            // 
+            this.slider2.Location = new System.Drawing.Point(528, 373);
+            this.slider2.Maximum = 255;
+            this.slider2.Name = "slider2";
+            this.slider2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.slider2.Size = new System.Drawing.Size(56, 355);
+            this.slider2.TabIndex = 6;
+            this.slider2.Value = 150;
+            this.slider2.Scroll += new System.EventHandler(this.slider2_Scroll);
+            // 
+            // slider3
+            // 
+            this.slider3.Location = new System.Drawing.Point(594, 373);
+            this.slider3.Maximum = 255;
+            this.slider3.Name = "slider3";
+            this.slider3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.slider3.Size = new System.Drawing.Size(56, 355);
+            this.slider3.TabIndex = 7;
+            this.slider3.Value = 110;
+            this.slider3.Scroll += new System.EventHandler(this.slider3_Scroll_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(656, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "[val]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(656, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "[val]";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(591, 333);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Threshold";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(594, 308);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(69, 22);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(591, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "[val]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(718, 738);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.slider3);
+            this.Controls.Add(this.slider2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.slider);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar slider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar slider2;
+        private System.Windows.Forms.TrackBar slider3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ EmguExample/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.IO.Ports;
+
+namespace EmguExample
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        SerialPort _serialPort = new SerialPort("COM6", 2400);   //***IMPORTANT***
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture();
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+
+            _serialPort.DataBits = 8;            //***IMPORTANT***
+            _serialPort.Parity = Parity.None;
+            _serialPort.StopBits = StopBits.Two;
+            _serialPort.Open();
+        }
+
+        private int dakka = 165;
+        private int red1 = 150;
+        private int red2 = 110;
+        private int redmode = 0;
+        private int redtime = 20;
+
+        const byte STOP = 0x7F;
+        const byte FLOAT = 0x0F;
+        const byte FORWARD = 0x6f;
+        const byte BACKWARD = 0x5F;
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+                CvInvoke.Resize(frame, frame, pictureBox1.Size);
+                CvInvoke.Flip(frame,frame,FlipType.Horizontal);
+                //CvInvoke.BitwiseNot(frame, frame); //XXXXXXXXX
+
+                Image<Gray, Byte> img = frame.ToImage<Gray, Byte>();
+                Image<Bgr, Byte> img2 = frame.ToImage<Bgr, Byte>();
+
+
+
+                byte left = FORWARD;    //***IMPORTANT***
+                byte right = FORWARD;
+
+
+
+                //Image<Gray, Byte> blues = imgBgr[0];
+
+                int redCount = 0;
+                for (int x = 0; x < img2.Width; x++)
+                {
+                    for (int y = 0; y < img2.Height; y++)
+                    {
+                        if (img2.Data[y, x, 2] >= red1 && img2.Data[y, x, 0] <= red2 && img2.Data[y, x, 1] <= red2)
+                        {
+                            redCount++;
+                            img2.Data[y, x, 2] = 255;
+                        }
+                        else
+                        {
+                            img2.Data[y, x, 2] = 0;
+                        }
+                        img2.Data[y, x, 0] = 0;
+                        img2.Data[y, x, 1] = 0;
+                    }
+                }
+                int redbox = 800;
+                //int redmode = 0;
+                if(redCount >= redbox)
+                {
+                    redmode = redtime;
+                    this.Invoke(new Action(() => label5.Text = "RED"));
+                    left = FORWARD;
+                    right = STOP;           //***IMPORTANT***
+                    move(left, right);
+                }
+                if (redmode >= 1)
+                {
+                    redmode--;
+                    this.Invoke(new Action(() => label5.Text = "RED"));
+                    left = FORWARD;
+                    right = STOP;              //***IMPORTANT***
+                    move(left, right);
+                }
+                else
+                {
+                    this.Invoke(new Action(() => label5.Text = "NULL"));
+                }
+
+                pictureBox2.Image = img2.Bitmap;
+
+                img = img.ThresholdBinary(new Gray(dakka), new Gray(255));
+
+                /*
+                img = img.SmoothGaussian(5);
+                img = (img.Sobel(1, 0, 5).AbsDiff(new Gray(0))
+                    + img.Sobel(0, 1, 5).AbsDiff(new Gray(0))).Convert<Gray, Byte>();
+                img = img.Canny(100, 200);
+                */
+
+                //img = img.Erode(1); //************Had Previously************
+
+                int white = img.CountNonzero()[0], tot = img.Width * img.Height;
+
+
+
+                int limit1 = (img.Width / 3) * 1;
+                int limit2 = (img.Width / 3) * 2;
+                //int limit15 = (img.Width / 5);
+                //int limit45 = (img.Width / 5) * 4;
+
+                this.Invoke(new Action(() => label2.Text = $"{dakka}"));
+                this.Invoke(new Action(() => label3.Text = $"{red1}"));
+                this.Invoke(new Action(() => label4.Text = $"{red2}"));
+                this.Invoke(new Action(() => label6.Text = $"{redmode}"));
+
+                int leftWhiteCount = 0;
+                for (int x = 0; x < limit1; x++)
+                {
+                    for (int y = 0; y < img.Height; y++)
+                    {
+                        if (img.Data[y, x, 0] == 255)
+                            leftWhiteCount++;
+                    }
+                }
+
+                int rightWhiteCount = 0;
+                for (int x = limit2; x < img.Width; x++)
+                {
+                    for (int y = 0; y < img.Height; y++)
+                    {
+                        if (img.Data[y, x, 0] == 255)
+                            rightWhiteCount++;
+                    }
+                }
+
+                int middleWhiteCount = 0;
+                for (int x = limit1; x < limit2; x++)
+                {
+                    for (int y = 0; y < img.Height; y++)
+                    {
+                        if (img.Data[y, x, 0] == 255)
+                            middleWhiteCount++;
+                    }
+                }
+
+                if(redmode < 1)
+                {
+                // > >
+                if(leftWhiteCount < rightWhiteCount && leftWhiteCount < middleWhiteCount)          //***IMPORTANT***
+                {
+                    this.Invoke(new Action(() => label1.Text = "Left"));
+                    left = STOP;
+                    right = FORWARD;              //***IMPORTANT***
+                    move(left, right);
+                }
+                // < >
+                else if(leftWhiteCount > rightWhiteCount && rightWhiteCount < middleWhiteCount)
+                {
+                    this.Invoke(new Action(() => label1.Text = "right"));
+                    left = FORWARD;
+                    right = STOP;           //***IMPORTANT***
+                    move(left, right);
+                }
+                else
+                {
+                    this.Invoke(new Action(() => label1.Text = "forward"));
+                    left = FORWARD;
+                    right = FORWARD;           //***IMPORTANT***
+                    move(left, right);
+                }
+                }
+                else
+                {
+                    this.Invoke(new Action(() => label1.Text = "RED"));
+                }
+
+
+
+                /*
+                if(white > (tot/2))
+                {
+                    BackColor = Color.FromArgb(255, 0, 0);
+                }
+                else
+                {
+                    BackColor = Color.FromArgb(0, 255, 0);
+                }
+                */
+
+                pictureBox1.Image = img.Bitmap;
+
+
+                //Image<Gray, Byte> img2 = img.Convert<Gray, Byte>();
+                //img = img.ThresholdBinary(new Gray(100), new Gray(255));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void slider_Scroll(object sender, EventArgs e)
+        {
+            dakka = slider.Value;
+        }
+
+        private void slider2_Scroll(object sender, EventArgs e)
+        {
+            red1 = slider2.Value;
+        }
+
+        private void slider3_Scroll(object sender, EventArgs e)
+        {
+            red2 = slider3.Value;
+        }
+
+        private void slider3_Scroll_1(object sender, EventArgs e)
+        {
+            red2 = slider3.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int value = (int)numericUpDown1.Value;
+            redtime = value;
+        }
+
+
+
+        
+        private void move(byte left, byte right)       //***IMPORTANT***
+        {
+            byte[] buffer = { 0x01, left, right };
+            _serialPort.Write(buffer, 0, 3);
+        }
+        
+    }
+}
